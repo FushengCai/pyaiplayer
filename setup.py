2b6d5defec86efcfde6638395d92c20757cd3168
FushengCai <178680870@qq.com>
2024-05-08 09:25:01 +0800

    initial commit.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyaiplayer",
+    description="A tool to let human play game against AI model",
+    version="0.1",
+    packages=find_packages(),
+
+)
